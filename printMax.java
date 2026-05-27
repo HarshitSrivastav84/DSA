c5b3ebaf05d487e4443488bc3c13c937e5aa9047
Harshit Srivastav <harshitsrivastav@gmail.com>
2026-05-27 11:31:36 +0530

    1

---
+++ printMax.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class printMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];;
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int max = arr[0];
+        for(int i=0; i<n; i++){
+            if(max < arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum element in the array is: " + max);
+    }
+}
