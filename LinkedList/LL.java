99933e8aa5fe5b81f6dcd53fe95a5e88f598f5c1
Harshit Srivastav <current-harshitsrivastav874@gmail.com>
2026-06-06 19:48:25 +0530

    ignore class files

--- LinkedList/LL.java
+++ LinkedList/LL.java
@@ -2,27 +2,73 @@ public class LL {
 
     private Node head;
     private Node tail;
-
     private int size;
 
-    // 
-    public LL(){
+    public LL() {
         this.size = 0;
     }
 
-
-
-    class Node{
+    class Node {
         private int value;
         private Node next;
 
         // Constructor for Node class
-        private Node(int value){
+        private Node(int value) {
             this.value = value;
         }
-        private Node(int value, Node next){
+
+        private Node(int value, Node next) {
             this.value = value;
             this.next = next;
         }
+    }
+
+    // To insert a new node at the beginning of the linked list
+    public void insertFirst(int val) {
+        Node newNode = new Node(val);
+        newNode.next = head;
+        head = newNode;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    // To display the elements of the linked list
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+        System.out.println();
+    }
+
+    // To insert a new node at the end of the linked list
+    public void insertLast(int val) {
+        Node newNode = new Node(val);
+
+        // If tail pointer is not present then we have to traverse
+        // Node temp = head;
+        // while(temp.next != null){
+        // temp = temp.next;
+        // }
+
+        if(tail == null){
+            System.out.println("List is empty");
+            System.out.println("Insert some value first");
+        }
+
+        tail.next = newNode;
+        newNode.next = null;
+        tail = newNode;
+        size++;
+    }
+
+    // To print size
+    public void printSize(){
+        System.out.println("The number of elements are: " + size);
     }
 }
--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,5 +1,16 @@
 public class Main{
     public static void main(String args[]){
         LL list1 = new LL(); 
+
+        list1.insertFirst(3);
+        list1.insertFirst(2);
+        list1.insertFirst(1);
+        list1.display();
+
+        list1.insertLast(4);
+        list1.insertLast(5);
+        list1.display();
+        
+        list1.printSize();
     }
 }
