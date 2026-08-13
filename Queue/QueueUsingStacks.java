adcaa51fd1b3739f543d3e1c3c351a585adc73e4
Harshit Srivastav <harshitsrivastav874@gmail.com>
2026-08-13 23:35:01 +0530

    queue using stacks

---
+++ Queue/QueueUsingStacks.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+public class QueueUsingStacks {
+    public static void main(String[] args) {
+
+
+
+        // Stack<Integer> stack = new Stack<>();
+        // stack.push(5);
+        // System.out.println(stack.pop());
+    }
+}
+
+class UsingStack {
+    private Stack<Integer> first;
+    private Stack<Integer> second;
+
+    UsingStack() {
+        first = new Stack<>();
+        second = new Stack<>();
+    }
+
+    public void add(int item) {
+        first.push(item);
+    }
+
+    public int remove() throws Exception{
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+        int removed = second.pop();
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+        return removed;
+    }
+
+    public int peek() throws Exception{
+        while (!first.isEmpty()) {
+            second.push(first.pop());
+        }
+        int peeked = second.peek();
+        while (!second.isEmpty()) {
+            first.push(second.pop());
+        }
+        return peeked;
+    }
+
+    public boolean isEmpty(){
+        return first.isEmpty();
+    }
+}
